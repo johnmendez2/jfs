75d075f45adf065a0980a5cd230999c46d3c2205
John Mendez <johnbvon@gmail.com>
2023-02-06 08:14:59 +0400

    new link changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router, Link, Route, Routes, useNavigate} from 'react-router-dom'
+import {HashRouter as Router, Link, Route, Routes, useNavigate} from 'react-router-dom'
 import {Home} from './Components/Home'
 import {Login} from './Components/Login'
 import {Signup} from './Components/Signup'
